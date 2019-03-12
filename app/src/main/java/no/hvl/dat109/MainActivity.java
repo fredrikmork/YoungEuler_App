1dafbe130d4cd9c7aa627876d3d2bdefdb54a51a
h571544 <fredrik.1997@hotmail.com>
2019-03-12 11:34:56 +0100

    lagt til bakgrunnsbilde

--- app/src/main/java/no/hvl/dat109/MainActivity.java
+++ app/src/main/java/no/hvl/dat109/MainActivity.java
@@ -2,6 +2,7 @@ package no.hvl.dat109;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = new Button()
     }
 }
