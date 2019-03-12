5fa329f45a7fb4c5e05a48cb234d21a32f15940c
h571544 <fredrik.1997@hotmail.com>
2019-03-12 11:54:00 +0100

    knappene linker

--- app/src/main/java/no/hvl/dat109/MainActivity.java
+++ app/src/main/java/no/hvl/dat109/MainActivity.java
@@ -1,7 +1,9 @@
 package no.hvl.dat109;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startBtn = new Button()
+        Button startBtn = findViewById(R.id.startButton);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QuestionClass.class));
+            }
+        });
+
+        Button helpBtn = findViewById(R.id.hjelpButton);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Hjelp.class));
+            }
+        });
+
     }
 }
