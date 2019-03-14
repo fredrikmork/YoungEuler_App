3641df924b8614278dcb744942a7838ce820f5da
h181226 <simengjerde@hotmail.com>
2019-03-14 08:37:29 +0100

    Laget ny Question-class

--- app/src/main/java/no/hvl/dat109/Question.java
+++ app/src/main/java/no/hvl/dat109/Question.java
@@ -1,23 +1,24 @@
 package no.hvl.dat109;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class QuestionClass extends AppCompatActivity {
+public class Question extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question);
+        setContentView(R.layout.activity_question);
 
         Button menyBtn = findViewById(R.id.menyButton);
         menyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(QuestionClass.this, MainActivity.class));
+                startActivity(new Intent(Question.this, MainActivity.class));
             }
         });
 
@@ -35,7 +36,7 @@ public class QuestionClass extends AppCompatActivity {
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(QuestionClass.this, Camera.class));
+                startActivity(new Intent(Question.this, Camera.class));
             }
         });
     }
