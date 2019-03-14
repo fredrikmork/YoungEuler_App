2384aa94043e2cb9d6283950f034fafce3a17f51
h571544 <fredrik.1997@hotmail.com>
2019-03-14 09:32:44 +0100

    Camera?

--- app/src/main/java/no/hvl/dat109/Question.java
+++ app/src/main/java/no/hvl/dat109/Question.java
@@ -1,6 +1,8 @@
 package no.hvl.dat109;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,13 @@ import android.widget.ImageView;
 
 public class Question extends AppCompatActivity {
 
+    public static final int CAMERA_REQUEST = 9999;
+    ImageView cameraBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
 
         Button menyBtn = findViewById(R.id.menyButton);
         menyBtn.setOnClickListener(new View.OnClickListener() {
@@ -32,12 +37,25 @@ public class Question extends AppCompatActivity {
             }
         });
 
-        ImageView cameraBtn = findViewById(R.id.cameraButton);
-        cameraBtn.setOnClickListener(new View.OnClickListener() {
+        cameraBtn = (ImageView) findViewById(R.id.cameraButton);
+        /*cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Question.this, Camera.class));
             }
-        });
+        });*/
+    }
+    public void OpenCamera(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivityForResult(intent, CAMERA_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode == CAMERA_REQUEST){
+            Bitmap bitmap = (Bitmap) (data.getExtras().get("data"));
+            cameraBtn.setImageBitmap(bitmap);
+        }
+
     }
 }
