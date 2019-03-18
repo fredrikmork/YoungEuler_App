c93dbd092a69dde71f0be0079aa16e00edcce7ab
h151274 <bonn_96aa@hotmail.com>
2019-03-18 14:57:20 +0100

    ny bakgrunn og bilde

--- app/src/main/java/no/hvl/dat109/MainActivity.java
+++ app/src/main/java/no/hvl/dat109/MainActivity.java
@@ -3,18 +3,8 @@ package no.hvl.dat109;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,27 +29,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        String url = "";
-        RequestQueue rq = Volley.newRequestQueue(this);
 
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.e("Rest response", response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Rest response", error.toString());
-                    }
-                }
-        );
-
-        rq.add(objectRequest);
     }
 }
