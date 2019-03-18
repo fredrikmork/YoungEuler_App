ea5b9b613519e796cbdadda616e24668fc010dda
h181226 <simengjerde@hotmail.com>
2019-03-18 11:09:23 +0100

    Laget utkast til metode for database

--- app/src/main/java/no/hvl/dat109/MainActivity.java
+++ app/src/main/java/no/hvl/dat109/MainActivity.java
@@ -3,8 +3,18 @@ package no.hvl.dat109;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Help.class));
             }
         });
+
+        String url = "";
+        RequestQueue rq = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest response", error.toString());
+                    }
+                }
+        );
+
 
     }
 }
